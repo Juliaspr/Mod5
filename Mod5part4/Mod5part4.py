87a53053f7eac995012f6794df24d59e638abcca
Juliaspr <79360641+Juliaspr@users.noreply.github.com>
2024-12-19 17:01:39 +0300

    done

---
+++ Mod5part4/Mod5part4.py
@@ -0,0 +1,45 @@
+import json
+class Model:
+    title = str
+    text = str
+    author = str
+
+
+    def __init__(self, title:str, text: str,  author: str):
+        self.title = title
+        self.author = author
+        self.text = text
+
+    def get_title(self):
+        return self.title
+
+    def set_title(self, title:str):
+        self.title = title
+
+    def get_text(self):
+        return self.text
+
+    def set_text(self, text: str):
+        self.text = text
+
+    def get_author(self):
+        return self.author
+
+    def set_author(self, author: str):
+        self.author = author
+
+
+    def save(self):
+        title = self.title
+        text = self.text
+        author = self.author
+        dictionary = {"title": title, "text ":  text, "author": author}
+        with open('1.json', 'a') as f:
+                json.dump(dictionary, f)
+
+s = Model("ggg", "fff", "eee")
+s.save()
+
+print(s.get_author(),s.get_title(),s.get_text())
+
+
